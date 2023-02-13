00e2ce005839ad869660d204c205a14c0acde34b
Nathaniel Richardson <ncrichardson15@gmail.com>
2023-02-13 13:44:22 -0500

    push before reformat

--- src/chunk.rs
+++ src/chunk.rs
@@ -13,6 +13,8 @@ pub struct Chunk {
     pub x: u32,
     /// The region z
     pub z: u32,
+    /// Contains the biome information for each section
+    biome_data: Option<[[String; 64]; 24]>,
 }
 
 impl Chunk {
@@ -24,10 +26,10 @@ impl Chunk {
     /// * `region` - The Region from which to get the Chunk
     /// * `chunk_x` - The x coordinate within the Region of the Chunk
     /// * `chunk_z` - The z coordinate within the Region of the Chunk
-    pub fn from_region(region: &Region, chunk_x: u32, chunk_z: u32) -> Option<Chunk> {
+    pub fn from_region(region: & Region, chunk_x: u32, chunk_z: u32) -> Option<Chunk> {
         match region.chunk_data(chunk_x, chunk_z) {
             Some(data) => {
-                return Some(Chunk{ data, x: chunk_x, z: chunk_z });
+                return Some(Chunk{ data, x: chunk_x, z: chunk_z, biome_data: None });
             }
             None => None,
         }
@@ -168,74 +170,6 @@ impl Chunk {
         None
     }
 
-    /// Returns the String representation of the biome for a Chunk. Chunks can have different biomes at different vertical sections so use a heightmap to determine the top section if you only want the surface biome.
-    /// 
-    /// # Arguments
-    /// 
-    /// * `y` - The y section of the chunk to get the biome of.
-    /// 
-    /// # Examples
-    /// 
-    /// ```rust,no_run
-    /// use simple_anvil::region::Region;
-    /// let region = Region::from_file("r.0.0.mca");
-    /// let chunk = region.get_chunk(0, 0).unwrap();
-    /// let heightmap = chunk.get_heightmap(false);
-    /// let y = if let Some(heights) = heightmap {
-    ///     heights.get(0).unwrap()
-    /// } else {
-    ///     panic!("Chunk not fully generated");
-    /// }
-    /// let section_y = ((y + 64) / 16 - 4) as i8
-    /// let biome = chunk.get_biome(section_y);
-    /// ```
-    /// 
-    /// ```rust,no_run
-    /// use simple_anvil::region::Region;
-    /// let region = Region::from_file("r.0.0.mca");
-    /// let chunk = region.get_chunk(0, 0).unwrap();
-    /// let biome = chunk.get_biome(-3);
-    /// ```
-    pub fn get_biome(&self, y: i32) -> String {
-        let sections = if let Value::List(s) = self.data.get("sections").unwrap() {
-            s
-        } else {
-            panic!("Value should be a list?")
-        };
-        for section in sections {
-            let section = if let Value::Compound(s) = section {
-                s
-            } else {
-                panic!("Should be a compound?")
-            };
-            let current_y = if let Value::Byte(val) = section.get("Y").unwrap() {
-                val
-            } else {
-                panic!("invalid height found")
-            };
-            if current_y == &(((y + 64) / 16 - 4) as i8) {
-                let biomes = if let Value::Compound(c) = section.get("biomes").unwrap() {
-                    c
-                } else {
-                    panic!("biomes not found")
-                };
-                let pallete = if let Value::List(l) = biomes.get("palette").unwrap() {
-                    l
-                } else {
-                    panic!("pallete not found")
-                };
-                let biome = if let Value::String(s) = &pallete[0] {
-                    s
-                } else {
-                    panic!("failed to get string")
-                };
-                return biome.to_string();
-            }
-            
-        };
-        return String::from("minecraft:ocean")
-    }
-
     /// Returns the block at a particular x, y, z coordinate within a chunk. x and z should be the coordinates within the Chunk (0-15).
     /// 
     /// # Examples
@@ -257,80 +191,31 @@ impl Chunk {
         let biomes = if let Some(Value::Compound(b)) = section.get("biomes") {
             b
         } else {
-            panic!("no biome?")
+            panic!("Biome portion of section missing")
         };
         let pal = if let Value::List(l) = biomes.get("palette").unwrap() {
             l
         } else {
-            panic!("naw2")
+            panic!("Biome palette missing")
         };
         let data_exists = biomes.get("data");
         let biome = match data_exists {
-            Some(dat) => {
-                let dat = if let Value::LongArray(la) = dat {
+            Some(data) => {
+                let data = if let Value::LongArray(la) = data {
                     la
                 } else {
-                    panic!("naw?")
+                    panic!("Failed to get biome data as long array")
                 };
-                let dat = dat[0];
-                let index = ((y % 16 / 4) * 16) + (z / 4) * 4 + (x / 4);
-                let bits = self.bit_length(pal.len() - 1);
-                let shifted_dat = dat >> ((bits as i32 * index) % 64);
-                if 64 - ((bits as i32 * index) % 64) < bits as i32 {
-                    todo!("I skipped this for now as it seemed a bit more tedious")
-                }
-                let pal_id = shifted_dat & ((2u32.pow(bits) - 1) as i64);
-                if let Value::String(s) = pal[pal_id as usize].to_owned() {
+                let dat = data[0];
+                let bin = format!("{:b}", dat);
+                // println!("{bin}, {}", bin.len());
+                let i = bin.chars().collect::<Vec<char>>()[(((y & 0xC) << 2) | (z & 0xC) | ((x & 0xC) >> 2)) as usize].to_digit(10).unwrap();
+                if let Value::String(s) = pal[i as usize].to_owned() {
                     s
                 } else {
                     panic!("hah")
                 }
-                // let bits = self.bit_length(pal.len() - 1); //cmp::max(self.bit_length(pal.len() - 1), 4);
-                // let index = y * 16 * 16 + z * 16 + x;
-                // let mut d = 0;
-                // let mut modified = false;
-                // if dat < 0 {
-                //     d = dat as u64;
-                //     modified = true;
-                // }
-                // let shifted_dat = (if modified { d as usize } else { dat as usize }) >> (index as usize % (64 / bits as usize) * bits as usize);
-                // let pal_id = shifted_dat & (2u32.pow(bits) - 1) as usize;
-                // println!("x: {}, z: {}, index: {}, {}", x, z, index % 32, pal_id);
-                // if let Value::String(s) = pal[pal_id].to_owned() {
-                //     s
-                // } else {
-                //     panic!("hah")
-                // }
-                // String::from("huh")
-                // let index = 128 * ((y + 64) % 16) + 16 * z + x;
-                // let index1 = (y * 16 * 16 + z * 16 + x) / 64;
-                // let index2 = (y * 16 * 16 + z * 16 + x) % 64;
-                // // println!("{} | {}", index1, index2);
-                // let options = format!("{:b}", dat[0]);
-                // // println!("{}", options);
-                // let chars: Vec<_> = options.chars().collect();
-                // let bits = self.bit_length(pal.len() - 1);
-                // let shifted = dat[0] >> ((128 * y + 16 * z + x) as usize % (64 / bits as usize)) as i64;
-                // let chars2: Vec<_> = format!("{:b}", shifted).chars().collect();
-                // let shifty = dat[0] >> index1;
-                // let shifty_chars = format!("{:b}", shifty).chars().collect::<Vec<char>>();
-                // println!("{}, {}", shifty_chars[index1 as usize], shifty_chars[shifty_chars.len() - 1 - index1 as usize]);
-                // println!("{}, {}", chars[options.len() - (index % 64) as usize - 1], chars2[options.len() - (index % 64) as usize - 1]);
-                // println!("{:b}", dat[0]);
-                // let index = z * 16 + x;
                 
-                
-                // println!("{:b}", shifted);
-                // let dat_shifted = (dat[0] as usize) >> (index as usize % (64 / bits as usize) * bits as usize);
-                // // println!("{:b}", dat_shifted);
-                // // let pal_id2 = shifted & (2u32.pow(bits as u32) - 1) as i64;
-                // let pal_id = dat_shifted & (2u32.pow(bits as u32) - 1) as usize;
-                // // println!("index: {}, data: {}, data_shifted: {}, bits: {}, id: {}, id2: {}", index, dat[0], dat_shifted, bits, pal_id, pal_id2);
-                // if let Value::String(s) = pal[pal_id].to_owned() {
-                //     s
-                // } else {
-                //     panic!("hah")
-                // }
             },
             None => {
                 pal[0].to_string()
@@ -354,7 +239,7 @@ impl Chunk {
 
         match block_states {
             Some(bs) => {
-                let bits = cmp::max(self.bit_length(palette.len() - 1), 4);
+                let bits = cmp::max(bit_length(palette.len() - 1), 4);
                 let index = y * 16 * 16 + z * 16 + x;
                 match bs.get("data") {
                     Some(data) => {
@@ -407,32 +292,60 @@ impl Chunk {
         
     }
 
-    /// Returns the bitlength of a usize value
-    fn bit_length(&self, num: usize) -> u32 {
-        // The number of bits that the number consists of, this is an integer and we don't care about signs or leading 0's
-        // 0001 and 1 have the same return value
-        // I think the lowest number that could come in is -1?
-        // usize is always returned from the len function so I think that it will only be usize?
-        if num == 0 {
-            return 0;
-        }
-        // Convert the number to a string version of the binary representation
-        // Get the number of leading 0's
-        let _leading = num.leading_zeros();
-        // Place the number into binary
-        let s_num = format!("{:b}", num);
-        // Remove leading 0's
-        // let s = &s_num[leading as usize..];
-        // Return the length
-        // Leading zeros appear to be removed when changed to bits
-        return s_num.len() as u32;
-    }
+    fn fill_biome_data(mut self) {
+        let mut biome_data = [[""; 64]; 24].map(|e| e.map(|se| se.to_string()));
+        for n in 0..24 {
+            let section = self.get_section(n).unwrap();
+            let biomes = if let Some(Value::Compound(b)) = section.get("biomes") {
+                b
+            } else {
+                panic!("Biome portion of section not found")
+            };
+            let biome_palette = if let Value::List(l) = biomes.get("palette").unwrap() {
+                l
+            } else {
+                panic!("Biome palette not found")
+            };
+            let biome_data_exists = biomes.get("data");
+            match biome_data_exists {
+                Some(data) => {
 
-    fn bin_append(self, a: u32, b: u32, length: Option<u32>) -> u32 {
-        let length = match length {
-            Some(l) => l,
-            None => self.bit_length(b as usize),
-        };
-        return (a << length) | b
+                },
+                None => {
+                    let thing = biome_palette[0].to_string();
+                    biome_data[n as usize] = [thing.as_str(); 64].map(|e| e.to_string());
+                }
+            }
+        }
+        self.biome_data = Some(biome_data);
     }
+}
+
+/// Returns the bitlength of a usize value
+fn bit_length(num: usize) -> u32 {
+    // The number of bits that the number consists of, this is an integer and we don't care about signs or leading 0's
+    // 0001 and 1 have the same return value
+    // I think the lowest number that could come in is -1?
+    // usize is always returned from the len function so I think that it will only be usize?
+    if num == 0 {
+        return 0;
+    }
+    // Convert the number to a string version of the binary representation
+    // Get the number of leading 0's
+    let _leading = num.leading_zeros();
+    // Place the number into binary
+    let s_num = format!("{:b}", num);
+    // Remove leading 0's
+    // let s = &s_num[leading as usize..];
+    // Return the length
+    // Leading zeros appear to be removed when changed to bits
+    return s_num.len() as u32;
+}
+
+fn bin_append(a: u32, b: u32, length: Option<u32>) -> u32 {
+    let length = match length {
+        Some(l) => l,
+        None => bit_length(b as usize),
+    };
+    return (a << length) | b
 }
